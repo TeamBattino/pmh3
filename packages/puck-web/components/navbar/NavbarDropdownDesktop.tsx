1bda34b5fcb95e4ecd05e482ae4f33e4852baf3d
Battino <battino46@gmail.com>
2026-04-13 01:00:38 +0200

    fix: update navbar dropdown styling

--- packages/puck-web/components/navbar/NavbarDropdownDesktop.tsx
+++ packages/puck-web/components/navbar/NavbarDropdownDesktop.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { NavbarDropdownArrowSvg } from "@pfadipuck/graphics/NavbarDropdownArrowSvg";
+import { Fragment, useState } from "react";
 import ClickAwayListener from "../../ui/ClickAwayListener";
 import { NavbarDropdownGroupedProps } from "./NavbarDropdown";
-import { Fragment, useState } from "react";
 
 export function NavbarDropdownDesktop({
   label,
@@ -24,11 +24,17 @@ export function NavbarDropdownDesktop({
       {open && (
         <ClickAwayListener onClickAway={() => setOpen(false)}>
           <div className="z-30 min-w-36 translate-y-4 drop-shadow-[0px_0px_1px_#000000] absolute items-center bg-white flex flex-col text-black font-rockingsoda text-2xl rounded-xl p-4 border-black b-2">
-            <NavbarDropdownArrowSvg
-              invertRotationDirection={true}
-              open={open}
-              className="fill-black"
-            />
+               <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 52.36 24.43"
+                className=" absolute w-10 h-10 -translate-x-8 -translate-y-[43.4px] z-20 pointer-events-none"
+              >
+                <path
+                  stroke="none"
+                  fill="white"
+                  d="M0,22c.18,0,15.99.01,17.77-21.53-.01-.5.67-.65.87-.2,1.72,4.02,5.6,11.55,11.83,15.62,8.27,5.38,16.59,6.13,21.89,6.13,0,.02-50.12-.02-52.36-.02Z"
+                />
+              </svg>
             {groupedItems.map((items, index) => (
               <Fragment key={index}>
                 <div className="flex w-full gap-2 items-center">
